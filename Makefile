f53ce894085e99c40c1f0080555e959ccaa26c1e
Pablo Illescas Ruiz <pillesca@c3r9s1.42malaga.com>
2024-04-19 12:58:21 +0200

    Añadida primera textura

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: pillesca <pillesca@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/17 18:46:03 by pillesca          #+#    #+#              #
-#    Updated: 2024/04/17 19:55:41 by pillesca         ###   ########.fr        #
+#    Updated: 2024/04/19 12:35:34 by pillesca         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,6 +24,8 @@ CC = gcc
 CFLAGS = -Wall -Wextra -Werror
 MLX42 = -LMLX42 -lMLX42 -Iinclude -lglfw -L"/Users/$(USER)/.brew/opt/glfw/lib/"
 
+all: $(NAME)
+
 $(NAME): $(OBJ) ./MLX42/libmlx42.a
 	$(CC) $(OBJ) $(CFLAGS) $(MLX42) -o $(NAME) 
 
@@ -33,3 +35,5 @@ clean:
 
 fclean: clean
 	$(RM) $(NAME)
+
+.PHONY: all, clean, fclean
--- so_long.c
+++ so_long.c
@@ -6,14 +6,33 @@
 /*   By: pillesca <pillesca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 19:54:51 by pillesca          #+#    #+#             */
-/*   Updated: 2024/04/17 20:05:01 by pillesca         ###   ########.fr       */
+/*   Updated: 2024/04/19 12:56:29 by pillesca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "MLX42/MLX42.h"
-#include <stdlib.h>
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+#include "so_long.h"
+
+void	error_exit(void	*mlx)
+{
+	if (mlx)
+		mlx_terminate(mlx);
+	exit(EXIT_FAILURE);
+}
+
+static void	draw_texture(void *mlx)
+{
+	mlx_texture_t	*t_sand;
+	mlx_image_t		*i_sand;
+
+	t_sand = mlx_load_png("./textures/sand.png");
+	if (!t_sand)
+		error_exit(mlx);
+	i_sand = mlx_texture_to_image(mlx, t_sand);
+	if (!i_sand)
+		error_exit(mlx);
+	if (mlx_image_to_window(mlx, i_sand, 0, 0) < 0)
+		error_exit(mlx);
+}
 
 int	main(void)
 {
@@ -21,8 +40,9 @@ int	main(void)
 
 	mlx = mlx_init(WINDOW_WIDTH, WINDOW_HEIGHT, "My Game", true);
 	if (!mlx)
-		return (1);
+		error_exit(mlx);
+	draw_texture(mlx);
 	mlx_loop(mlx);
-	free (mlx);
+	mlx_terminate(mlx);
 	return (0);
 }
